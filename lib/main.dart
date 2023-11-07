0dfd342ac9f11eba300183642429a848f5aedc39
alazarlemma02 <alazar03lemma@gmail.com>
2023-11-07 23:10:12 +0300

    wrapped material app to add a multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:advanced_bloc/demo.dart';
+import 'package:advanced_bloc/features/welcome/bloc/welcome_bloc.dart';
+import 'package:advanced_bloc/features/welcome/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +12,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const Demo(),
-      debugShowCheckedModeBanner: false,
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => WelcomeBloc(),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+            useMaterial3: true,
+          ),
+          home: const WelcomeScreen(),
+          debugShowCheckedModeBanner: false,
+        ));
   }
 }
